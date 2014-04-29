0f59acc933cec014d58b6dbb3ca00b625aac2dfc
twh_eastasia <twh_eastasia@163.com>
2014-04-29 09:15:48 +0800

    modify notedatabase

--- src/com/twh/simplenote/DatabaseHelper.java
+++ src/com/twh/simplenote/DatabaseHelper.java
@@ -14,20 +14,22 @@ public class DatabaseHelper {
 	public static final String KEY_TITLE = "title";
 	public static final String KEY_BODY = "body";
 	public static final String KEY_ROWID = "_id";
-	public static final String KEY_CREATED = "created";
-
-	private Diarydatabase mDbHelper;
-	public static String DATABASE_NAME="dairydatabase";
-	public static String DATABASE_TABLE="diary";
+	public static final String KEY_CREATED = "created_at";
+	public static final String KEY_UPDATED = "updated_at";
+	public static final String KEY_TYPE = "type";
+	
+	private Notedatabase mDbHelper;
+	public static String DATABASE_NAME="notedatabase";
+	public static String DATABASE_TABLE="notes";
 	public static int version=1;
-	public static String DATABASE_CREATE= "create table diary (_id integer primary key autoincrement, "
-		+ "title text not null, body text not null, created text not null);";
+	public static String DATABASE_CREATE= "create table notes (_id integer primary key autoincrement, "
+		+ "title text not null, body text not null, type text not null, created_at text not null, updated_at text not null);";
 	private SQLiteDatabase mDb;
 	private final Context mCtx;
 	
 
-	private class Diarydatabase extends SQLiteOpenHelper{//私有类
-		public Diarydatabase(Context context) {
+	private class Notedatabase extends SQLiteOpenHelper{//私有类
+		public Notedatabase(Context context) {
 			super(context, DATABASE_NAME, null, version);
 			// TODO Auto-generated constructor stub
 		}
@@ -40,7 +42,7 @@ public class DatabaseHelper {
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			// TODO Auto-generated method stub
-			db.execSQL("DROP TABLE IF EXISTS diary");
+			db.execSQL("DROP TABLE IF EXISTS notes");
 			onCreate(db);
 		}
 	}
@@ -50,7 +52,7 @@ public class DatabaseHelper {
 	}
 
 	public DatabaseHelper open() throws SQLException {
-		mDbHelper = new Diarydatabase(mCtx);
+		mDbHelper = new Notedatabase(mCtx);
 		mDb = mDbHelper.getWritableDatabase();
 		return this;
 	}
@@ -59,45 +61,51 @@ public class DatabaseHelper {
 		mDbHelper.close();
 	}
 	
-	
-	public long creatediary(String title,String body){
-		ContentValues initvalues=new ContentValues();
-		Calendar calendar=Calendar.getInstance();
+	//insert
+	public long createNote(String title,String body, String type){
+		ContentValues initvalues = new ContentValues();
+		Calendar calendar = Calendar.getInstance();
 		initvalues.put(KEY_TITLE, title);
 		initvalues.put(KEY_BODY, body);
-		String createdtime=calendar.get(Calendar.YEAR)+"年"+calendar.get(Calendar.MONTH)+"月"+calendar.get(Calendar.DAY_OF_MONTH)+"日"
-			+calendar.get(Calendar.HOUR_OF_DAY)+"时"+calendar.get(Calendar.MINUTE)+"分";
+		initvalues.put(KEY_TYPE, type);
+		String createdtime = calendar.get(Calendar.YEAR)+"-"+calendar.get(Calendar.MONTH)+"-"+calendar.get(Calendar.DAY_OF_MONTH)+" "
+			+calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE)+":" + calendar.get(Calendar.SECOND);
 		initvalues.put(KEY_CREATED, createdtime);
+		initvalues.put(KEY_UPDATED, createdtime);
 		return mDb.insert(DATABASE_TABLE,null,initvalues);
 		
 	}
 	
-	public boolean updatediary(long rowid,String title,String body){
-		ContentValues initvalues=new ContentValues();
-		Calendar calendar=Calendar.getInstance();
+	//update
+	public boolean updateNote(long rowid,String title,String body, String type){
+		ContentValues initvalues = new ContentValues();
+		Calendar calendar = Calendar.getInstance();
 		initvalues.put(KEY_TITLE, title);
 		initvalues.put(KEY_BODY, body);
-		String createdtime=calendar.get(Calendar.YEAR)+"年"+calendar.get(Calendar.MONTH)+"月"+calendar.get(Calendar.DAY_OF_MONTH)+"日"
-			+calendar.get(Calendar.HOUR_OF_DAY)+"时"+calendar.get(Calendar.MINUTE)+"分";
-		initvalues.put(KEY_CREATED, createdtime);
-		return mDb.update(DATABASE_TABLE,initvalues,KEY_ROWID+"="+rowid,null)>0;
+		initvalues.put(KEY_TYPE, type);
+		String createdtime = calendar.get(Calendar.YEAR)+"-"+calendar.get(Calendar.MONTH)+"-"+calendar.get(Calendar.DAY_OF_MONTH)+" "
+			+calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE)+ ":" + calendar.get(Calendar.SECOND);
+		initvalues.put(KEY_UPDATED, createdtime);
+		return mDb.update(DATABASE_TABLE, initvalues, KEY_ROWID + "=" + rowid, null)>0;
 	}
 	
-	public boolean deleteDiary(long rowId) {
+	//delete
+	public boolean deleteNote(long rowId) {
 
 		return mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
 	}
 	
+	//get all notes
 	public Cursor getAllNotes() {
 
 		return mDb.query(DATABASE_TABLE, new String[] { KEY_ROWID, KEY_TITLE,
-				KEY_BODY, KEY_CREATED }, null, null, null, null, null);
+				KEY_BODY, KEY_TYPE, KEY_CREATED, KEY_UPDATED }, null, null, null, null, null);
 	}
 
-	public Cursor getDiary(long rowId) throws SQLException {
+	public Cursor getNote(long rowId) throws SQLException {
 
 		Cursor mCursor = mDb.query(true, DATABASE_TABLE, new String[] { KEY_ROWID, KEY_TITLE,
-				KEY_BODY, KEY_CREATED }, KEY_ROWID + "=" + rowId, null, null,
+				KEY_BODY, KEY_TYPE, KEY_CREATED, KEY_UPDATED }, KEY_ROWID + "=" + rowId, null, null,
 				null, null, null);
 		if (mCursor != null) {
 			mCursor.moveToFirst();
